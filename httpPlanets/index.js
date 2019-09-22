63efc4d8c4daf0e7c0150787c4aec38d6563c44a
Roman Batsenko <rbatsenko@gmail.com>
2019-09-22 18:15:34 +0200

    Add waking up request

--- httpPlanets/index.js
+++ httpPlanets/index.js
@@ -2,22 +2,38 @@ const axios = require('axios');
 
 module.exports = async function(context, req) {
   context.log('HTTP trigger function processed a request.');
+  context.log(req.wakeUp);
 
   try {
-    const response = await axios.get('https://swapi.co/api/planets/');
-    const requestDate = new Date();
-    const formattedDate = `${requestDate.getDate()}.${requestDate.getMonth()}.${requestDate.getFullYear()}@${requestDate.getHours()}:${requestDate.getMinutes()}:${requestDate.getSeconds()}:${requestDate.getMilliseconds()}`;
-    context.log(`Fetched planets from SWAPI at ${formattedDate}`);
-    context.res = {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: {
-        status: true,
-        data: response.data.results,
-      },
-    };
+    if (context.req.method === 'GET') {
+      const response = await axios.get('https://swapi.co/api/planets/');
+      const requestDate = new Date();
+      const formattedDate = `${requestDate.getDate()}.${requestDate.getMonth()}.${requestDate.getFullYear()}@${requestDate.getHours()}:${requestDate.getMinutes()}:${requestDate.getSeconds()}:${requestDate.getMilliseconds()}`;
+      context.log(`Fetched planets from SWAPI at ${formattedDate}`);
+      context.res = {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: {
+          status: true,
+          data: response.data.results,
+        },
+      };
+    } else if (context.req.method === 'POST') {
+      if (context.req.body.wakeUp) {
+        context.res = {
+          status: 200,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: {
+            status: true,
+            data: { wakingUp: true },
+          },
+        };
+      }
+    }
   } catch (error) {
     context.log(error);
     context.res = {
